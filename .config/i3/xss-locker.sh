1226b2896d3aebac63ad4cc4d247fd6218111599
takelley1 <hxyz@protonmail.com>
2020-01-12 09:09:05 -0500

    Change lock background to black

--- .config/i3/xss-locker.sh
+++ .config/i3/xss-locker.sh
@@ -3,4 +3,4 @@
 # screen locker for waking up from suspend (see ~/.config/i3/config)
 # requires the i3lock-color package
 
-i3lock --blur=5 --show-failed-attempts --indicator --force-clock --radius 250 --ring-width 20  --timecolor=ffffffff --verifcolor=ffffffff --wrongcolor=ffffffff --layoutcolor=ffffffff --datecolor=ffffffff --datestr="%a  %Y-%m-%d" --timepos="(w/2):(h/2.5)"
+i3lock --show-failed-attempts --indicator --force-clock --radius 250 --ring-width 20  --color=000000 --timecolor=ffffffff --verifcolor=ffffffff --wrongcolor=ffffffff --layoutcolor=ffffffff --datecolor=ffffffff --datestr="%a  %Y-%m-%d" --timepos="(w/2):(h/2.5)"
