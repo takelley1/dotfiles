c0ddd2e2ab7f4036645f70aece2216e68be7c7e7
takelley1 <hxyz@protonmail.com>
2020-01-20 19:05:01 -0500

    Force L and R channels to balance if out of sync

--- .config/i3/volume-up.sh
+++ .config/i3/volume-up.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# increases volume, then checks to see if both L and R channels are in sync
+# if channels are not in sync, forces R channel to equal L channel
+
 amixer sset Master,0 2%+,2%+
+
+L=$(amixer get Master | grep -o '[0-9]*%' | sed -n 1p | tr -d '%')
+R=$(amixer get Master | grep -o '[0-9]*%' | sed -n 2p | tr -d '%')
+
+if [ $L != $R ]
+then
+	amixer sset Master,0 $L%,$L%
+fi
 
 exit 0
