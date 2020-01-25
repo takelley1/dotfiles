cc8a71771b321921d4d6165d3be19a559f050978
takelley1 <hxyz@protonmail.com>
2020-01-25 15:01:39 -0500

    Compress unused files into archive

--- .config/i3/unused-files/brightness/brightness-reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xrandr --output DP-1 --brightness 1 --gamma 1:1:1 &> /dev/null
-xrandr --output DVI-I-1 --brightness 1 --gamma 1:1:1 &> /dev/null
-xrandr --output HDMI-0 --brightness 1 --gamma 1:1:1 &> /dev/null
-
-exit 0
--- .config/i3/unused-files/screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-import -window root ~/latest-screenshot_$(date +%H:%M:%S_%d-%m-%Y).png
-
-mv ~/latest-screenshot*.png ~/Pictures/misc_images/screenshots/uby-desktop/ 
-
-exit 0
