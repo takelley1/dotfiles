393f152c6b90b7fef383d7d4fd2133b3c4534f60
takelley1 <hxyz@protonmail.com>
2020-01-27 19:47:14 -0500

    Change i3locker to bar-style from circle-style

--- .config/i3/xss-locker.sh
+++ .config/i3/xss-locker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# screen locker for waking up from suspend (see ~/.config/i3/config)
+# standard screen locker that can be invoked manually or after waking from suspend (see ~/.config/i3/config)
 # requires the i3lock-color package
 
-i3lock --show-failed-attempts --indicator --force-clock --radius 250 --ring-width 20  --color=000000 --timecolor=ffffffff --verifcolor=ffffffff --wrongcolor=ffffffff --layoutcolor=ffffffff --datecolor=ffffffff --datestr="%a  %Y-%m-%d" --timepos="(w/2):(h/2.5)"
+i3lock --show-failed-attempts --bar-indicator --bar-max-height 25 --bar-step 25 --bar-periodic-step 25 --force-clock --color=000000 --timecolor=ffffffff --verifcolor=ffffffff --wrongcolor=ffffffff --layoutcolor=ffffffff --datecolor=ffffffff --datestr="%a  %Y-%m-%d" --timestr="%H:%H" --timepos="(w/2):(h/2.5)"
