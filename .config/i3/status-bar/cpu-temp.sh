ff544bd4af739e58e3bd2f75b64974eb56769534
takelley1 <hxyz@protonmail.com>
2020-02-05 19:38:42 -0500

    Notify user with dunst when cpu > 75 deg C

--- .config/i3/status-bar/cpu-temp.sh
+++ .config/i3/status-bar/cpu-temp.sh
@@ -4,15 +4,35 @@
 
 if [ $(hostname) == "deimos" ]
 then
-	echo '' $(sensors -u | grep temp1_input | awk {'print $2'} | cut -f 1 -d '.')'°C'
+	TEMP=$(sensors -u | grep temp1_input | awk {'print $2'} | cut -f 1 -d '.')
+	echo '' $TEMP'°C'
+	# if the temperature goes above a certain value, send an alert notification
+	# requires the dunst notification daemon to be running
+	if [ $TEMP -gt 75 ]
+	then
+		notify-send "CPU is getting hot! ($TEMP°C)"
+		sleep 20s
+	fi
 
 elif [ $(hostname) == "phobos" ]
 then
-	echo '' $(sensors -u | grep temp1_input | awk {'print $2'} | cut -f 1 -d '.')'°C'
+	TEMP=$(sensors -u | grep temp1_input | awk {'print $2'} | cut -f 1 -d '.')
+	echo '' $TEMP'°C'
+	if [ $TEMP -gt 75 ]
+	then
+		notify-send "CPU is getting hot! ($TEMP°C)"
+		sleep 20s
+	fi
 
 elif [ $(hostname) == "tethys" ]
 then
-	echo ' ' $(sensors -u | grep -A 1 'Package id 0' | tail -1 | awk {'print $2'} | cut -f 1 -d '.')'°C'
+	TEMP=$(sensors -u | grep -A 1 'Package id 0' | tail -1 | awk {'print $2'} | cut -f 1 -d '.')
+	echo ' ' $TEMP'°C'
+	if [ $TEMP -gt 75 ]
+	then
+		notify-send "CPU is getting hot! ($TEMP°C)"
+		sleep 20s
+	fi
 fi
 
 exit 0
