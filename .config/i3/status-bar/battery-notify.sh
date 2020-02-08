eb402d3adf20414d7a178ec0a664faca9633bdb8
takelley1 <hxyz@protonmail.com>
2020-02-08 13:21:57 -0500

    Reorder battery notifications so most urgent take priority

--- .config/i3/status-bar/battery-notify.sh
+++ .config/i3/status-bar/battery-notify.sh
@@ -5,23 +5,30 @@
 
 # get current battery percentage
 PERCENT=$(acpi -b | awk {'print $4'} | tr -d '%' | tr -d ',')
+
+# get current battery state (charging or discharging)
 STATE=$(acpi -b | awk {'print $3'})
 
 # don't send a notification if the battery is being charged
 if [ $STATE == 'Charging,' ]
-then
-	exit 0
+  then
+    exit 0
 fi
 
-if [ $PERCENT -lt 20 ]
-then
-	notify-send "Battery under 20%"
+if [ $PERCENT -lt 5 ]
+  then
+    notify-send -u critical "BATTERY UNDER 5%"
+    exit 0
+
 elif [ $PERCENT -lt 10 ]
-then
-	notify-send "Battery under 10%"
-elif [ $PERCENT -lt 5 ]
-then
-	notify-send -u critical "BATTERY UNDER 5%"
+  then
+    notify-send "Battery under 10%"
+    exit 0
+
+elif [ $PERCENT -lt 20 ]
+  then
+    notify-send "Battery under 20%"
+    exit 0
 fi
 
 exit 0
