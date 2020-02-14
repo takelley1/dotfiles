b8372709c7a13318c64296b1a09795064c128a5f
takelley1 <hxyz@protonmail.com>
2020-02-13 20:30:00 -0500

    Possible fix for fish variable overwriting

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -43,18 +43,10 @@ function fish_prompt --description 'Write out the prompt'
     switch "$USER"
         # root gets red colors
         case root toor admin
-            set -U fish_color_host -o ff0000
-            set -U fish_color_user -o ff0000
-            set -U fish_color_cwd -o 729fcf
-            set suffix '#'
+            echo -n -s (set_color ff000) "$USER" $normal @ (set_color ff000) (prompt_hostname) $normal ':' (set_color 729fcf) (pwd) $normal (__fish_vcs_prompt) $normal "# "
         # everyone else gets green colors
         case '*'
-            set -U fish_color_host -o 8ae234
-            set -U fish_color_user -o 8ae234
-            set -U fish_color_cwd -o 729fcf
-            set suffix '>'
+            echo -n -s (set_color 8ae234) "$USER" $normal @ (set_color 8ae234) (prompt_hostname) $normal ':' (set_color 729fcf) (pwd) $normal (__fish_vcs_prompt) $normal "> "
     end
-
-echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ':' (set_color $fish_color_cwd) (pwd) $normal (__fish_vcs_prompt) $normal $suffix " "
 
 end
