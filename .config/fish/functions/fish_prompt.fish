41e1c2aca5bbc897ed313a1c150e6da6f78298a9
takelley1 <hxyz@protonmail.com>
2020-02-13 19:59:55 -0500

    Adjust fish prompt colors to match bash

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -47,21 +47,16 @@ function fish_prompt --description 'Write out the prompt'
     set -l suffix
     switch "$USER"
         case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
+            set -U fish_color_host -o ff0000
+            set -U fish_color_user -o ff0000
+            set -U fish_color_cwd -o 729fcf
             set suffix '#'
         case '*'
-            set color_cwd $fish_color_cwd
+            set -U fish_color_host -o 8ae234
+            set -U fish_color_user -o 8ae234
+            set -U fish_color_cwd -o 729fcf
             set suffix '>'
     end
 
-    set -l prompt_status
-    if test $last_status -ne 0
-        set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
-    end
-
-    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status $suffix " "
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ':' (set_color $fish_color_cwd) (pwd) $normal (__fish_vcs_prompt) $normal $prompt_status $suffix " "
 end
