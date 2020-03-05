a72bd9ec8e40588e247c14837f6081ede9f9a88c
takelley1 <hxyz@protonmail.com>
2020-03-04 22:22:44 -0500

    Make update-notify.sh check if on arch-based distro

--- .config/i3/scripts/status-bar/update-notify.sh
+++ .config/i3/scripts/status-bar/update-notify.sh
@@ -3,8 +3,12 @@
 # status bar script to print to the status bar if a file called "0pacman" is being run
 # this is to notify the user if the hourly cron pacman update job is running so they don't reboot while it's happening
 
-if [[ ! -z $(ps aux | grep '0pacman'| grep -v 'grep') ]]; then
-    printf "%s\n" "performing automatic updates ... DO NOT REBOOT"
+# check if the system uses pacman
+if [[ -x "/usr/bin/pacman" ]]; then
+    # check if the pacman updater is running
+    if [[ ! -z $(ps aux | grep '0pacman'| grep -v 'grep') ]]; then
+        printf "%s\n" "performing automatic updates ... DO NOT REBOOT"
+    fi
 fi
 
 exit 0
