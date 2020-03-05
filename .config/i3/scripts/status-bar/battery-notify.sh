99ae35bcf4f22f5df013c27743141f8db395a933
takelley1 <hxyz@protonmail.com>
2020-03-04 22:46:17 -0500

    Improve battery-notify.sh to reflect current practices

--- .config/i3/scripts/status-bar/battery-notify.sh
+++ .config/i3/scripts/status-bar/battery-notify.sh
@@ -4,25 +4,25 @@
 # this script is called from the i3blocks config file periodically
 
 # get current battery percentage
-percent=$(acpi -b | awk {'print $4'} | tr -d '%' | tr -d ',')
+percent=$(acpi -b | awk '{print $4}' | tr -d '%' | tr -d ',')
 
 # get current battery state (charging or discharging)
-state=$(acpi -b | awk {'print $3'})
+state=$(acpi -b | awk '{print $3}')
 
 # don't send a notification if the battery is being charged
-if [[ $state == 'Charging,' ]]; then
+if [[ ${state} == 'Charging,' ]]; then
     exit 0
 fi
 
-if [[ $percent -lt 5 ]]; then
+if [[ ${percent} -lt 5 ]]; then
     notify-send -u critical "Battery under 5%"
     exit 0
 
-elif [[ $percent -lt 10 ]]; then
+elif [[ ${percent} -lt 10 ]]; then
     notify-send -u critical "Battery under 10%"
     exit 0
 
-elif [[ $percent -lt 15 ]]; then
+elif [[ ${percent} -lt 15 ]]; then
     notify-send "Battery under 15%"
     exit 0
 
