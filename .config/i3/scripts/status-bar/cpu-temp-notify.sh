e3d0e0661ba7b4de7a587f190a5fecf0ee39899d
T. Austin Kelley <hxyz@protonmail.com>
2020-03-12 14:20:41 -0400

    Improve syntax of cpu-temp-notify.sh

--- .config/i3/scripts/status-bar/cpu-temp-notify.sh
+++ .config/i3/scripts/status-bar/cpu-temp-notify.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# status bar script that notifies the user of a high CPU temp
-# this script is called from the i3blocks config file periodically
+# Status bar script that notifies the user of a high CPU temperature.
 
 if [[ ${HOSTNAME} == "deimos" ]] || [[ ${HOSTNAME} == "phobos" ]]; then
 
-        temp=$(sensors -u | grep temp1_input | awk {'print $2'} | cut -f 1 -d '.')
+        temp=$(sensors -u | grep 'temp1_input' | awk '{print $2}' | cut -f 1 -d '.')
 
-        if [ ${temp} -gt 85 ]; then
+        if [[ ${temp} -gt 85 ]]; then
                 notify-send -u critical "CPU is getting VERY hot! (${temp}°C)"
 	        exit 0
-        elif [ ${temp} -gt 75 ]; then
+        elif [[ ${temp} -gt 75 ]]; then
                 notify-send "CPU is getting hot! (${temp}°C)"
 	        exit 0
         fi
@@ -18,12 +17,12 @@ if [[ ${HOSTNAME} == "deimos" ]] || [[ ${HOSTNAME} == "phobos" ]]; then
 
 elif [[ ${HOSTNAME} == "tethys" ]]; then
 
-	temp=$(sensors -u | grep -A 1 'Package id 0' | tail -1 | awk {'print $2'} | cut -f 1 -d '.')
+	temp=$(sensors -u | grep -A 1 'Package id 0' | tail -1 | awk '{print $2}' | cut -f 1 -d '.')
 
-        if [ ${temp} -gt 85 ]; then
+        if [[ ${temp} -gt 85 ]]; then
                 notify-send -u critical "CPU is getting VERY hot! (${temp}°C)"
 	        exit 0
-        elif [ ${temp} -gt 75 ]; then
+        elif [[ ${temp} -gt 75 ]]; then
                 notify-send "CPU is getting hot! (${temp}°C)"
 	        exit 0
         fi
