099b28480a86ca829e59b83fff7ccce73ef8a39b
T. Austin Kelley <hxyz@protonmail.com>
2020-03-12 14:30:08 -0400

    Change double negative "if" statement in timeshift-notify.sh
    Instead of "if ! -z", you can just do "if -n", which is the intended syntax to check if a statement returns text.

--- .config/i3/scripts/status-bar/timeshift-notify.sh
+++ .config/i3/scripts/status-bar/timeshift-notify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# status bar script to print to the status bar if timeshift is running
-# this is to notify the user if the daily timeshift snapshot job is running so they don't reboot while it's happening
+# Status bar script to print a notification if Timeshift is running.
+# This is to notify the user if the daily timeshift snapshot job is running so they don't reboot while it's happening.
 
-if [[ ! -z $(ps aux | grep 'timeshift' | grep -v 'grep' | grep -iE 'rsync|btrfs') ]]; then
-    printf "%s\n" "managing snapshots ..."
+if [[ -n $(ps aux | grep 'timeshift' | grep -v 'grep' | grep -iE 'rsync|btrfs') ]]; then
+    printf "%s\n" "managing snapshots ... DO NOT REBOOT"
 fi
 
 exit 0
