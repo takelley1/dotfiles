b5aea3589d1c99d56652e0e98ba1f6f538925239
T. Austin Kelley <hxyz@protonmail.com>
2020-03-12 14:22:40 -0400

    Improve syntax of cpu-temp.sh

--- .config/i3/scripts/status-bar/cpu-temp.sh
+++ .config/i3/scripts/status-bar/cpu-temp.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# status bar script for obtaining the current CPU package temp
+# Status bar script for printing the current CPU package temperature.
 
-if [ ${HOSTNAME} == "deimos" ] || [[ ${HOSTNAME} == "phobos" ]]; then
+if [[ ${HOSTNAME} == "deimos" ]] || [[ ${HOSTNAME} == "phobos" ]]; then
 
-    temp=$(sensors -u | grep temp1_input | awk {'print $2'} | cut -f 1 -d '.')
+    temp=$(sensors -u | grep 'temp1_input' | awk '{print $2}' | cut -f 1 -d '.')
     printf "%s\n" " ${temp}°C"
 
 
-elif [[ $(hostname) == "tethys" ]]; then
+elif [[ ${HOSTNAME} == "tethys" ]]; then
 
-    temp=$(sensors -u | grep -A 1 'Package id 0' | tail -1 | awk {'print $2'} | cut -f 1 -d '.')
+    temp=$(sensors -u | grep -A 1 'Package id 0' | tail -1 | awk '{print $2}' | cut -f 1 -d '.')
     printf "%s\n" " ${temp}°C"
 
 fi
