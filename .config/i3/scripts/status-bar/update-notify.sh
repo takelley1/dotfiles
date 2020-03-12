9300cfbb3ec793097a9ddd518c0ec7c29da72947
T. Austin Kelley <hxyz@protonmail.com>
2020-03-12 14:27:23 -0400

    Combine "if" statements on update-notify.sh

--- .config/i3/scripts/status-bar/update-notify.sh
+++ .config/i3/scripts/status-bar/update-notify.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# status bar script to print to the status bar if a file called "0pacman" is being run
-# this is to notify the user if the hourly cron pacman update job is running so they don't reboot while it's happening
+# Status bar script to print a notification if a process called "0pacman" is running.
+# This is to notify the user if the hourly cron pacman update job is running so they don't reboot while it's happening.
 
-# check if the system uses pacman
-if [[ -x "/usr/bin/pacman" ]]; then
-    # check if the pacman updater is running
-    if [[ ! -z $(ps aux | grep '0pacman'| grep -v 'grep') ]]; then
+# Check if (1) the system uses pacman and (2) the pacman cron job is running.
+if [[ -x "/usr/bin/pacman" ]] && [[ -n $(ps aux | grep '0pacman'| grep -v 'grep') ]]; then
         printf "%s\n" "performing automatic updates ... DO NOT REBOOT"
-    fi
 fi
 
 exit 0
