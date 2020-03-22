7a52a0f55da5016d3da780d2207b0c2ddeb9a6d6
takelley1 <hxyz@protonmail.com>
2020-03-22 15:07:11 -0400

    Clean up xss-locker.sh

--- .config/i3/scripts/xss-locker.sh
+++ .config/i3/scripts/xss-locker.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-# standard screen locker that can be invoked manually or after waking from suspend (see ~/.config/i3/config)
-# requires the i3lock-color package
+# Standard screen locker that can be invoked manually or after waking from suspend (see ~/.config/i3/config).
+# Requires the i3lock-color package.
 
-i3lock --show-failed-attempts --bar-indicator --bar-max-height 25 --bar-step 25 --bar-periodic-step 25 --force-clock --color=000000 --timecolor=ffffffff --verifcolor=ffffffff --wrongcolor=ffffffff --layoutcolor=ffffffff --datecolor=ffffffff --datestr="%a  %Y-%m-%d" --timestr="%H:%H" --timepos="(w/2):(h/2.5)"
+i3lock                       \
+    --show-failed-attempts   \
+    --bar-indicator          \
+    --bar-max-height 25      \
+    --bar-step 25            \
+    --bar-periodic-step 25   \
+    --force-clock            \
+    --color=000000           \
+    --timecolor=ffffffff     \
+    --verifcolor=ffffffff    \
+    --wrongcolor=ffffffff    \
+    --layoutcolor=ffffffff   \
+    --datecolor=ffffffff     \
+    --datestr="%a  %Y-%m-%d" \
+    --timestr="%H:%H"        \
+    --timepos="(w/2):(h/2.5)"
