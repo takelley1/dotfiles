abe8f24548d7a1a7ec6104f81dd6cebfd4c97b8f
takelley1 <hxyz@protonmail.com>
2020-03-22 15:07:22 -0400

    Reduce volume up/down increment to 1%

--- .config/i3/scripts/volume-down.sh
+++ .config/i3/scripts/volume-down.sh
@@ -3,7 +3,7 @@
 # Decreases volume, then checks to see if L and R channels are equal.
 # If channels are not equal, forces R channel to equal L channel.
 
-amixer sset Master,0 2%-,2%-
+amixer sset Master,0 1%-,1%-
 
 L=$(amixer get Master | grep -Eo '[0-9]{1,3}%' | sed -n 1p | tr -d '%')
 R=$(amixer get Master | grep -Eo '[0-9]{1,3}%' | sed -n 2p | tr -d '%')
