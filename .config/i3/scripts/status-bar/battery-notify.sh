c3ebe841ab4550f6b5731c25ab4813f9ad28fe84
takelley1 <hxyz@protonmail.com>
2020-03-23 00:58:01 -0400

    Modify battery-notify notification urgency

--- .config/i3/scripts/status-bar/battery-notify.sh
+++ .config/i3/scripts/status-bar/battery-notify.sh
@@ -14,12 +14,16 @@ state=$(acpi -b | awk '{print $3}')
 # Don't send a notification if the battery is being charged.
 [[ ${state} == 'Charging,' ]] && exit 0
 
-if [[ ${percent} -lt 5 ]]; then
+if [[ ${percent} -lt 3 ]]; then
+    notify-send -u critical "Battery under 3%"
+    exit 0
+
+elif [[ ${percent} -lt 5 ]]; then
     notify-send -u critical "Battery under 5%"
     exit 0
 
 elif [[ ${percent} -lt 10 ]]; then
-    notify-send -u critical "Battery under 10%"
+    notify-send "Battery under 10%"
     exit 0
 
 elif [[ ${percent} -lt 15 ]]; then
