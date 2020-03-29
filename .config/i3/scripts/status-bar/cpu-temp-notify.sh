06611c980329f679081753ae58760e7bcb465a65
takelley1 <hxyz@protonmail.com>
2020-03-29 16:26:22 -0400

    Change cpu temp notify thresholds, force system suspend at 90C

--- .config/i3/scripts/status-bar/cpu-temp-notify.sh
+++ .config/i3/scripts/status-bar/cpu-temp-notify.sh
@@ -12,10 +12,14 @@ else
     temp=$(sensors -u | grep 'temp1_input' | awk '{print $2}' | cut -f 1 -d '.')
 fi
 
-if [[ ${temp} -gt 85 ]]; then
+if [[ ${temp} -gt 90 ]]; then
+    notify-send -u critical "Suspending system due to CPU temp! (${temp}°C)"
+    systemctl suspend
+    exit 0
+elif [[ ${temp} -gt 85 ]]; then
     notify-send -u critical "CPU is getting VERY hot! (${temp}°C)"
     exit 0
-elif [[ ${temp} -gt 75 ]]; then
+elif [[ ${temp} -gt 80 ]]; then
     notify-send "CPU is getting hot! (${temp}°C)"
     exit 0
 fi
