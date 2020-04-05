45f834737a8c4251f2fa38782bd9b934549f895a
takelley1 <hxyz@protonmail.com>
2020-04-04 20:11:16 -0400

    Reduce length of status bar notifications slightly

--- .config/i3/scripts/status-bar/timeshift-notify.sh
+++ .config/i3/scripts/status-bar/timeshift-notify.sh
@@ -3,6 +3,6 @@
 # Status bar script to print a notification if Timeshift is running.
 # This is to notify the user if the daily timeshift snapshot job is running so they don't reboot while it's happening.
 
-[[ -n $(pgrep 'timeshift') ]] && printf "%s\n" "managing snapshots ... DO NOT REBOOT"
+[[ -n $(pgrep 'timeshift') ]] && printf "%s\n" "managing snapshots - DO NOT REBOOT"
 
 exit 0
