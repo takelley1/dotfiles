65b2d5a7ee95ad2f82bc9a93e4015a88ba0fa39b
takelley1 <hxyz@protonmail.com>
2020-04-13 00:27:52 -0400

    Reformat cpu-temp-notify.sh

--- .config/i3/scripts/status-bar/cpu-temp-notify.sh
+++ .config/i3/scripts/status-bar/cpu-temp-notify.sh
@@ -3,27 +3,24 @@
 # Status bar script that notifies the user of a high CPU temperature.
 
 # Don't run on phobos.
-if [[ ${HOSTNAME} == "phobos" ]]; then
-    exit 0
+if [[ "${HOSTNAME}" == "phobos" ]]; then
+  exit 0
 fi
 
 # Get the current temp from a file, which was written to by the cpu-temp.py script.
 temp=$(cat /tmp/cputemp)
 
-if [[ ${temp} -gt 90 ]]; then
-    notify-send -u critical "Suspending system due to CPU temp! (${temp}°C)"
-    systemctl suspend
-    exit 0
+if [[ "${temp}" -ge 90 ]]; then
+  notify-send -u critical "Suspending system due to CPU temp! (${temp}°C)"
+  systemctl suspend &
+  exit 0
 
-elif [[ ${temp} -gt 85 ]]; then
-    notify-send -u critical "CPU is getting VERY hot! (${temp}°C)"
-    exit 0
+elif [[ "${temp}" -ge 85 ]]; then
+  notify-send -u critical "CPU is getting VERY hot! (${temp}°C)"
+  exit 0
 
-elif [[ ${temp} -gt 82 ]]; then
-    notify-send "CPU is getting hot! (${temp}°C)"
-    exit 0
-
-else
+elif [[ "${temp}" -ge 82 ]]; then
+  notify-send "CPU is getting hot! (${temp}°C)"
   exit 0
 
 fi
