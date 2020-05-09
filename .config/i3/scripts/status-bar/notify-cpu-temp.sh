0010510dafc2f0bec883e3855ce8cf5c126c12fd
takelley1 <hxyz@protonmail.com>
2020-05-09 18:54:09 -0400

    Increase temp limits for cpu notification script

--- .config/i3/scripts/status-bar/notify-cpu-temp.sh
+++ .config/i3/scripts/status-bar/notify-cpu-temp.sh
@@ -10,16 +10,12 @@ fi
 # Get the current temp from a file, which was written to by the cpu-temp.py script.
 temp=$(cat /tmp/cputemp)
 
-if [[ "${temp}" -ge 90 ]]; then
+if [[ "${temp}" -ge 91 ]]; then
   notify-send -u critical "Suspending system due to CPU temp! (${temp}°C)"
   systemctl suspend &
   exit 0
 
-elif [[ "${temp}" -ge 85 ]]; then
-  notify-send -u critical "CPU is getting hot! (${temp}°C)"
-  exit 0
-
-elif [[ "${temp}" -ge 83 ]]; then
+elif [[ "${temp}" -ge 87 ]]; then
   notify-send "CPU is getting hot! (${temp}°C)"
   exit 0
 
