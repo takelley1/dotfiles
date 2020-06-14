a8dbffcc18bc359b328ccaf2c8b1b77a236de57d
takelley1 <hxyz@protonmail.com>
2020-06-14 00:14:23 -0400

    Replace load.sh with a python script

---
+++ .config/i3/scripts/status-bar/load.py
@@ -0,0 +1,26 @@
+#!/bin/python
+
+# Status bar script for printing the current system load, as seen in `top`.
+
+# Emoji U+1F4CA 📈
+# Font-Awesome f3fd 
+
+import psutil
+import sys
+
+load_tuple = psutil.getloadavg()
+load_list = []
+
+# Break apart the tuple in order to add leading zeroes to each number.
+for load in load_tuple:
+    load = "{:05.2f}".format(load)
+    # Reassemble the values into a list.
+    load_list.append(load)
+
+# Convert the list into a string and remove unnecessary characters.
+load_list = str(load_list)
+load_list = load_list.replace('[','').replace(']','').replace("'","")
+
+print('', load_list)
+
+sys.exit(0)
