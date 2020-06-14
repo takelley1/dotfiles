bafe579e040042d8e5d2c1625566a7fd967133ca
takelley1 <hxyz@protonmail.com>
2020-06-14 00:24:20 -0400

    Change cpu.py to use python for string formatting

--- .config/i3/scripts/status-bar/cpu.py
+++ .config/i3/scripts/status-bar/cpu.py
@@ -7,31 +7,28 @@ import sys
 import psutil
 
 def main():
+    core_list = []
+    # Break up the tuple containing the usage of each CPU core.
+    for core_percent in psutil.cpu_percent(interval=0.5, percpu=True):
+      core_percent = round(core_percent)
 
-  core_list = []
+      # Add leading zeroes so all numbers are three digits.
+      core_percent = ("{:03d}".format(core_percent))
+      core_percent = (core_percent + "%")
+      # Create a list using the rounded values.
+      core_list.append(core_percent)
 
-  # Get a list of the current usage of each CPU core.
-  for core_percent in psutil.cpu_percent(interval=0.5, percpu=True):
-    # Round each value.
-    core_percent = round(core_percent)
-    # Add leading zeroes so all numbers are three digits.
-    core_percent = ("{:03d}".format(core_percent))
-    core_percent = (core_percent + "%")
-    # Re-create the list using the rounded values.
-    core_list.append(core_percent)
+    # Format output.
+    core_list = str(core_list)
+    core_list = core_list.replace("'", "")
+    core_list = core_list.replace(",", "")
+    core_list = core_list.replace("[", "")
+    core_list = core_list.replace("]", "")
+    core_list = core_list.replace("]", "")
+    core_list = core_list.replace(" ", "  ")
 
-  core_list = str(core_list)
-
-  # Format output.
-  os.system('echo ' + core_list + \
-          ' | sed -e "s/\[//g"    \
-                  -e "s/\]//g"    \
-                  -e "s/.*/ &/g" \
-                  -e "s/\,/ /g"   \
-                  >/tmp/cpu_usage')
-
-  print(open("/tmp/cpu_usage").read())
-  sys.exit(0)
+    print('', core_list)
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
