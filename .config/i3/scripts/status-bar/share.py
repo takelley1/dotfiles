641a125c8bc974da97fdd27ef6043abd504d02ac
takelley1 <hxyz@protonmail.com>
2020-06-14 00:37:01 -0400

    Replace share.sh with share.py

---
+++ .config/i3/scripts/status-bar/share.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+# Status bar script for printing the amount of gigabytes free in the network share, assuming it's mounted to /mnt/tank/share/documents.
+
+# Font-Awesome f6ff 
+
+import sys
+import psutil
+
+def main():
+    # Get free space in bytes
+    disk = psutil.disk_usage('/mnt/tank/share/documents').free
+    # Convert to TB
+    disk = disk / (1024**4)
+    disk = round(disk, 2)
+
+    print(' ' + str(disk) + "T")
+
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
