2a6293fd060af7763e97fc67abd76df76806ae71
takelley1 <hxyz@protonmail.com>
2020-06-13 23:36:53 -0400

    Fix load.sh sometimes missing leading zero

--- .config/i3/scripts/status-bar/load.sh
+++ .config/i3/scripts/status-bar/load.sh
@@ -9,14 +9,14 @@ load1m=$(awk '{print $1}' /proc/loadavg)
 load5m=$(awk '{print $2}' /proc/loadavg)
 load15m=$(awk '{print $3}' /proc/loadavg)
 
-# Add a leading zero to each value.
-if [[ $(printf "%.f\n" "${load1m}") -lt 10 ]]; then
+# Add a leading zero to each value if under 10.
+if [[ $(cut -f 1 -d '.' "${load1m}") -lt 10 ]]; then
   load1m=$(printf "%s\n" "0${load1m}")
 fi
-if [[ $(printf "%.f\n" "${load5m}") -lt 10 ]]; then
+if [[ $(cut -f 1 -d '.' "${load5m}") -lt 10 ]]; then
   load5m=$(printf "%s\n" "0${load5m}")
 fi
-if [[ $(printf "%.f\n" "${load15m}") -lt 10 ]]; then
+if [[ $(cut -f 1 -d '.' "${load15m}") -lt 10 ]]; then
   load15m=$(printf "%s\n" "0${load15m}")
 fi
 
