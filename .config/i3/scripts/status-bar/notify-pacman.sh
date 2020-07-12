e58fc7cc165dab14fae8196842138da16f761554
takelley1 <hxyz@protonmail.com>
2020-07-12 15:12:37 -0400

    Update pacman notification for new anacron update job

--- .config/i3/scripts/status-bar/notify-pacman.sh
+++ .config/i3/scripts/status-bar/notify-pacman.sh
@@ -1,9 +1,7 @@
 #!/bin/dash
 
-# Status bar script to print a notification if a process called "0pacman" is running.
-# This is to notify the user if the hourly cron pacman update job is running so they don't reboot while it's happening.
+# Notify the user if the daily anacron pacman update job is running so they don't reboot while it's happening.
 
-# Check if (1) the system uses pacman and (2) the pacman cron job is running.
-[ -x "/usr/bin/pacman" ] && [ -n "$(pgrep '0pacman')" ] && printf "%s\n" "performing updates - DO NOT REBOOT"
+[ -x "/usr/bin/pacman" ] && [ -n "$(pgrep -u root -f 'pacman')" ] && printf "%s\n" "Running pacman - DO NOT REBOOT"
 
 exit 0
