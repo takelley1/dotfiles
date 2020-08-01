0e3a0f7321562ca92ab07449b7652c66f66b616a
takelley1 <hxyz@protonmail.com>
2020-08-01 16:41:32 -0400

    Add -- to rm in screenshot.sh

--- .config/i3/scripts/screenshot.sh
+++ .config/i3/scripts/screenshot.sh
@@ -9,6 +9,6 @@ time="$(date +%Y-%m-%d_%H:%M:%S)"
 import -window root "/tmp/screenshot_${time}.png"
 notify-send -u low "Saved screenshot to ~/Pictures/${time}_$(hostname)_screenshot.png."
 pngcrush -s "/tmp/screenshot_${time}.png" "${HOME}/Pictures/${time}_$(hostname)_screenshot.png"
-rm -f "/tmp/screenshot_${time}.png"
+rm -f -- "/tmp/screenshot_${time}.png"
 
 exit 0
