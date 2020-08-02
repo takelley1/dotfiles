55633350480d7711bf8ad870dedc5f44cff4fdd0
takelley1 <hxyz@protonmail.com>
2020-08-01 22:28:54 -0400

    Improve battery status script "charging" formatting

--- .config/i3/scripts/status-bar/battery.py
+++ .config/i3/scripts/status-bar/battery.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
-
+#
 # Print battery percentage and estimated time remaining to full charge or
-# depletion.
+#   depletion.
 
 import psutil
 import os
 import sys
-
-# Don't run on Tethys, since it doesn't have a battery.
-if os.uname().nodename == 'tethys':
-    exit(0)
 
 # Convert seconds to HH:mm format.
 def secs2hours(secs):
@@ -21,23 +17,34 @@ def main():
     # Get battery information.
     batt = psutil.sensors_battery()
 
+    # Exit quietly if no battery is present.
+    if not hasattr(batt, 'percent'):
+        sys.exit(0)
+
     # Round remaining percent to nearest whole number.
     percent = round(batt.percent)
 
     # Convert remaining time to hours and minutes.
     remaining = secs2hours(batt.secsleft)
+    # Format remaining time with parentheses.
     remaining = str(remaining)
-
-    output=''
+    remaining = '(' + remaining + ')'
 
     # Indicate when the battery is being charged.
     if batt.power_plugged == True:
-        output = '(+)'
+        plug = ''
+    else:
+        plug=''
+
+    # This remaining time is caused by a full battery and is likely
+    #   a bug, so don't print anything.
+    if remaining == '(-1:59)':
+        remaining = ''
 
     if percent >= 99:
         output='  FULL'
     else:
-        output=' ' + str(percent) + '% (' + (str(remaining) + ')' ) + output
+        output=' ' + str(percent) + '% ' + remaining + plug
 
     print(output)
 
