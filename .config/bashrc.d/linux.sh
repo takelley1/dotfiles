93bf0c2688f13df63c17c46faf883eff9d4e74a0
takelley1 <hxyz@protonmail.com>
2020-08-10 14:18:20 -0400

    Add func to stop automounts

--- .config/bashrc.d/linux.sh
+++ .config/bashrc.d/linux.sh
@@ -4,37 +4,41 @@
 
 if [[ "${OSTYPE}" == "linux"* ]]; then
 
-  export SHELL="/bin/bash"
+    export SHELL="/bin/bash"
 
-  alias ls='ls --classify --color=auto --human-readable'
-  alias l='ls'
-  alias lr='ls --classify --color=auto --human-readable --reverse'
-  alias lsr='lr'
+    function stop-automount() {
+        systemctl stop "$(systemctl | awk '{ORS=" "}; /^  mnt.*automount/ {print $1}')" 2>/dev/null
+    }
 
-  # Show single-column.
-  alias ll='ls --classify --color=auto --human-readable -l'
+    alias ls='ls --classify --color=auto --human-readable'
+    alias l='ls'
+    alias lr='ls --classify --color=auto --human-readable --reverse'
+    alias lsr='lr'
 
-  alias llr='ls --classify --color=auto --human-readable -l --reverse'
-  alias la='ls --classify --color=auto --human-readable -l --all'
-  alias lar='ls --classify --color=auto --human-readable -l --all --reverse'
+    # Show single-column.
+    alias ll='ls --classify --color=auto --human-readable -l'
 
-  # Sort by size.
-  alias lss='ls --classify --color=auto --human-readable -l --all -S'
+    alias llr='ls --classify --color=auto --human-readable -l --reverse'
+    alias la='ls --classify --color=auto --human-readable -l --all'
+    alias lar='ls --classify --color=auto --human-readable -l --all --reverse'
 
-  alias lcon='ls -lZ --all --reverse'
-  alias untar='tar -xzvf'
+    # Sort by size.
+    alias lss='ls --classify --color=auto --human-readable -l --all -S'
 
-  if hash nvim 2>/dev/null; then
-    export EDITOR="/usr/bin/nvim"
-    export VISUAL="/usr/bin/nvim"
-    export SUDO_EDITOR="/usr/bin/nvim"
-  elif hash vim 2>/dev/null; then
-    export EDITOR="/usr/bin/vim"
-    export VISUAL="/usr/bin/vim"
-    export SUDO_EDITOR="/usr/bin/vim"
-  else
-    export EDITOR="/usr/bin/vi"
-    export VISUAL="/usr/bin/vi"
-    export SUDO_EDITOR="/usr/bin/vi"
-  fi
+    alias lcon='ls -lZ --all --reverse'
+    alias untar='tar -xzvf'
+
+    if hash nvim 2>/dev/null; then
+        export EDITOR="/usr/bin/nvim"
+        export VISUAL="/usr/bin/nvim"
+        export SUDO_EDITOR="/usr/bin/nvim"
+    elif hash vim 2>/dev/null; then
+        export EDITOR="/usr/bin/vim"
+        export VISUAL="/usr/bin/vim"
+        export SUDO_EDITOR="/usr/bin/vim"
+    else
+        export EDITOR="/usr/bin/vi"
+        export VISUAL="/usr/bin/vi"
+        export SUDO_EDITOR="/usr/bin/vi"
+    fi
 fi
