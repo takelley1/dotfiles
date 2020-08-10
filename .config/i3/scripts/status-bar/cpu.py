63cd1a61027335ebfcbed215e50eee30c4baba22
takelley1 <hxyz@protonmail.com>
2020-08-10 15:33:29 -0400

    Update cpu.py to express value in increments of 10

--- .config/i3/scripts/status-bar/cpu.py
+++ .config/i3/scripts/status-bar/cpu.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 #
 # Status bar script for printing the current usage of each CPU core.
+#
+# Usage is expressed in increments of 10, so a core value of "05" means
+#   the core is 50% used.
 
 import sys
 import psutil
@@ -9,11 +12,16 @@ import psutil
 def main():
     core_list = []
     # Break up the tuple containing the usage of each CPU core.
-    for core_percent in psutil.cpu_percent(interval=3, percpu=True):
+    for core_percent in psutil.cpu_percent(interval=2, percpu=True):
         core_percent = round(core_percent)
 
         # Add leading zeroes so all numbers are three digits.
         core_percent = ('{:03d}'.format(core_percent))
+
+        # Remove trailing digit to make more compact.
+        core_percent = str(core_percent)
+        core_percent = '{:.2}'.format(core_percent)
+
         # Create a list using the rounded values.
         core_list.append(core_percent)
 
@@ -26,7 +34,7 @@ def main():
     core_list = core_list.replace(']', '')
     core_list = core_list.replace(' ', '  ')
 
-    print('', core_list, ' %')
+    print('', core_list)
     sys.exit(0)
 
 
