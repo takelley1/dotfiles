96787030bcf517ce8f9320fb358daf654031dd4a
takelley1 <hxyz@protonmail.com>
2020-08-11 21:15:46 -0400

    Update git aliases

--- .config/bashrc.d/aliases-git.sh
+++ .config/bashrc.d/aliases-git.sh
@@ -4,6 +4,7 @@
 # See: https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 
 if [[ "${OSTYPE}" == "linux"* ]]; then
+    # "dot" for "dotfiles".
     alias dot='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 elif [[ "${OSTYPE}" == "freebsd"* ]]; then
     # FreeBSD's Git binary is at a different path than on Linux.
@@ -11,42 +12,55 @@ elif [[ "${OSTYPE}" == "freebsd"* ]]; then
 fi
 
 # Easy editing and committing of todo list.
-alias gcut='cd ~/notes/ && git commit -m "Update todo" ./todo.md && git push'
-alias todo='cd ~/notes/ && git pull && nvim ./todo.md && gcut'
+alias todo='cd ~/notes/ && git pull && nvim ./todo.md && git commit -m "Update todo.md" ./todo.md && git push'
 
-# "dot" for "dotfiles".
+# -----------------------------------------------------------------------------
+
 alias d='dot'
 alias da='dot add'
-alias daa='dot add -A ./'
-alias dau='dot add -u ./'
+alias daa='dot add --all ./'
+alias daaa='dot add --all'
+alias dau='dot add --update ./'
+alias dauu='dot add --update'
 alias db='dot branch'
 alias dc='dot commit'
-alias dcm='dot commit --message'
+alias dcam='dot commit --all --message'
 alias dch='dot checkout'
-alias ddi='dot diff'
+alias dcm='dot commit --message'
+alias ddd='dot diff'
+alias dds='dot diff --staged'
 alias dl='dot log'
 alias dm='dot merge'
 alias dp='dot push'
 alias dpu='dot pull'
-alias drm='dot rm'
 alias dr='dot restore'
+alias drm='dot rm'
 alias drs='dot restore --staged'
 alias ds='dot status --untracked-files=no'
 alias dss='dot status'
+function dcamp {
+dot commit --all --message "${@}" && dot push
+}
+function dcmp {
+dot commit --message "${@}" && dot push
+}
+
+# -----------------------------------------------------------------------------
 
 alias g='git'
 alias ga='git add'
-# Stage all modified files.
-alias gau='git add -u ./'
-# Stage all changes below the given path, including added or deleted files.
-alias gaa='git add -A ./'
+alias gaa='git add --all ./' # Stage all changes below current path.
+alias gaaa='git add --all'   # Stage all changes.
+alias gau='git add --update ./' # Add all modified files below current path.
+alias gauu='git add --update'   # Add all modified files.
 alias gb='git branch'
 alias gc='git commit'
-alias gcm='git commit --message'
+alias gca='git commit --all'
+alias gcam='git commit --all --message'
 alias gch='git checkout'
+alias gcm='git commit --message'
 alias gd='git diff'
-alias gdi='git diff'
-alias gf='git fetch'
+alias gds='git diff --staged'
 alias gl='git log'
 alias gp='git push'
 alias gpu='git pull'
@@ -54,3 +68,9 @@ alias gr='dot restore'
 alias grm='git rm'
 alias grs='dot restore --staged'
 alias gs='git status'
+function gcamp {
+git commit --all --message "${@}" && git push
+}
+function gcmp {
+git commit --message "${@}" && git push
+}
