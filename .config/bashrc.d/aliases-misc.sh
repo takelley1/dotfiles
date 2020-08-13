a175975cc59f915d5f4387a46eaa973cf39b3a27
takelley1 <hxyz@protonmail.com>
2020-08-13 14:42:50 -0400

    Add alias to disable proxy

--- .config/bashrc.d/aliases-misc.sh
+++ .config/bashrc.d/aliases-misc.sh
@@ -2,6 +2,8 @@
 #
 # Aliases for common utilities and apps.
 
+# UTILITIES -------------------------------------------------------------------
+
 # "Up 1 directory."
 alias u='cd ../'
 alias u2='cd ../../'
@@ -14,17 +16,21 @@ alias c='clear'
 alias mv='mv -v'
 alias cp='cp -v'
 
-# This is required for bash aliases to work with sudo.
-alias sudo='sudo '
+alias sudo='sudo ' # This is required for bash aliases to work with sudo.
 
-# Show text in terminal even after quitting less.
-alias less='less -XRF' 
+alias less='less -XRF' # Show text in terminal even after quitting less.
 alias grep='grep --color=always'
-alias mkdir='mkdir -pv'
+alias mkdir='mkdir -pv' # Always make parent directories.
+
+# PROXIES ---------------------------------------------------------------------
+
+alias noproxy='export http_proxy=; export https_proxy=; export HTTP_PROXY=; export HTTPS_PROXY=' # Disables proxy.
 
 alias pip='pip --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org'
 alias pip3='pip3 --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org'
 alias pip3.8='pip3.8 --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org'
+
+# APPS ------------------------------------------------------------------------
 
 alias ta='tmux -f ~/.config/tmux/tmux.conf attach'
 alias tmux='tmux -f ~/.config/tmux/tmux.conf'
@@ -34,9 +40,10 @@ alias vim='nvim'
 
 alias define='dict'
 alias eve='bash /opt/evesetup/lib/evelauncher/evelauncher.sh &'
-alias osrs='bash ~/scripts/bash/linux/osrs.sh &'
-  alias runescape='osrs'
-  alias rs='osrs'
-  alias snip='scrot --quality 100 --select --freeze --silent'
-  alias s='bash ~/scripts/bash/linux/ocvbot-sync-manual.sh'
 alias audible='bash /opt/OpenAudible/OpenAudible &'
+
+alias osrs='bash ~/scripts/bash/linux/osrs.sh &'
+    alias runescape='osrs'
+    alias rs='osrs'
+    alias snip='scrot --quality 100 --select --freeze --silent'
+    alias s='bash ~/scripts/bash/linux/ocvbot-sync-manual.sh'
