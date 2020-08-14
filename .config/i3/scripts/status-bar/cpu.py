ae5e5ed7509b60fcb943b5730ea1d82ee271dca5
takelley1 <hxyz@protonmail.com>
2020-08-13 21:33:27 -0400

    Change cpu status script to only show 4 most active cores

--- .config/i3/scripts/status-bar/cpu.py
+++ .config/i3/scripts/status-bar/cpu.py
@@ -12,7 +12,7 @@ import psutil
 def main():
     core_list = []
     # Break up the tuple containing the usage of each CPU core.
-    for core_percent in psutil.cpu_percent(interval=2, percpu=True):
+    for core_percent in psutil.cpu_percent(interval=1, percpu=True):
         core_percent = round(core_percent)
 
         # Add leading zeroes so all numbers are three digits.
@@ -24,6 +24,11 @@ def main():
 
         # Create a list using the rounded values.
         core_list.append(core_percent)
+
+    # Sort list by most active cores.
+    core_list.sort(reverse=True)
+    # Only show the 4 most active cores.
+    del core_list[4:]
 
     # Format output.
     core_list = str(core_list)
