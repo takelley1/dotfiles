b03cd1d634243061237d99eab3785e9146b6f3c8
takelley1 <hxyz@protonmail.com>
2020-08-14 00:21:42 -0400

    Increase font size and bar height on screen locker

--- .config/i3/scripts/xss-locker.sh
+++ .config/i3/scripts/xss-locker.sh
@@ -4,11 +4,12 @@
 # Requires the i3lock-color package.
 
 i3lock                       \
+    --ignore-empty-password  \
     --show-failed-attempts   \
     --bar-indicator          \
-    --bar-max-height 25      \
-    --bar-step 25            \
-    --bar-periodic-step 25   \
+    --bar-max-height 300     \
+    --bar-step 300           \
+    --bar-width=40           \
     --force-clock            \
     --color=000000           \
     --timecolor=ffffffff     \
@@ -16,6 +17,12 @@ i3lock                       \
     --wrongcolor=ffffffff    \
     --layoutcolor=ffffffff   \
     --datecolor=ffffffff     \
-    --datestr="%a  %Y-%m-%d" \
+    --datestr="%A, %Y-%m-%d" \
+    --datepos="(w/2):(h/2.5)"\
+    --date-font=sans-serif   \
+    --datesize=30            \
     --timestr="%H:%H"        \
-    --timepos="(w/2):(h/2.5)"
+    --timepos="(w/2):(h/3)"  \
+    --time-font=sans-serif   \
+    --timesize=40            \
+    --refresh-rate=10
