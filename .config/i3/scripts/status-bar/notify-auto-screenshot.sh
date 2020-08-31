b2897695836d581512150efa62df9fc77ba436d8
takelley1 <hxyz@protonmail.com>
2020-08-31 00:25:15 -0400

    Update auto-screenshot notification script for new name

--- .config/i3/scripts/status-bar/notify-auto-screenshot.sh
+++ .config/i3/scripts/status-bar/notify-auto-screenshot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env dash
 #
-# Status bar script to print a notification if auto-screenshot.sh is NOT running.
+# Status bar script to print a notification if asd.sh isn't running.
 
-if [ -z "$(pgrep -f "${HOME}/scripts/bash/linux/systemd/auto-screenshot\.sh$")" ]; then
+if [ -z "$(pgrep -f "${HOME}/scripts/bash/linux/systemd/asd\.sh$")" ]; then
     printf "%s\n" "Screenshot script not running!"
 fi
 
