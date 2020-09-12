dcfc9f7ed9f44590ab1e09311afd015942da1470
takelley1 <hxyz@protonmail.com>
2020-09-12 17:02:34 -0400

    Add icon for indicating updates in status bar script

--- .config/i3/scripts/status-bar/packages.sh
+++ .config/i3/scripts/status-bar/packages.sh
@@ -2,30 +2,33 @@
 #
 # Status bar script for printing the total number of packages on the system.
 # The number of packages that require updates is shown in parentheses.
-#   (requires passwordless sudo for pacman).
 #
 # Font-Awesome f49e 
+# Font-Awesome  fa-arrow-circle-up [&#xf0aa;]
 
 if [ -x "/usr/bin/pacman" ]; then
 
     updates=$(checkupdates 2>/dev/null | wc -l)
 
-    # Add parenthesis around the update number if there are updates to apply.
-    if [ "${updates}" -gt 0 ]; then
-        updates=" (${updates})"
-    # If there aren't any updates, make sure the variable is empty.
-    else
-        updates=""
-    fi
-
     # Show Font-Awesome icons if possible, use text everywhere else.
     if [ -n "$(ls /usr/share/fonts/OTF/Font\ Awesome*.otf)" ]; then
-        symbol=""
+        pkg_symbol=""
+        update_symbol=" "
     else
-        symbol="PKGs"
+        pkg_symbol="PKGs"
+        update_symbol=" "
     fi
 
-    printf "%s\n" "${symbol} $(pacman -Q | wc -l)${updates}"
+    # Add parenthesis around the update number if there are updates to apply.
+    if [ "${updates}" -gt 0 ]; then
+        updates="(${updates})"
+    # If there aren't any updates, make the relevant variables empty.
+    else
+        updates=""
+        update_symbol=" "
+    fi
+
+    printf "%s\n" "${pkg_symbol} $(pacman -Q | wc -l)${update_symbol}${updates}"
 
 elif [ -x "/usr/bin/apt" ]; then
     printf "%s\n" "PKGs $(dpkg-query --list | wc -l)"
