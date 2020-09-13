1baedf3e94f1fb0c40cc1aa5ecc86f4fffa6cc1c
takelley1 <hxyz@protonmail.com>
2020-09-13 16:06:41 -0400

    Update formatting of packages.sh

--- .config/i3/scripts/status-bar/packages.sh
+++ .config/i3/scripts/status-bar/packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env dash
 #
 # Status bar script for printing the total number of packages on the system.
-# The number of packages that require updates is shown in parentheses.
+# The number of packages requiring updates is also shown.
 #
 # Font-Awesome f49e 
 # Font-Awesome  fa-arrow-circle-up [&#xf0aa;]
@@ -13,19 +13,16 @@ if [ -x "/usr/bin/pacman" ]; then
     # Show Font-Awesome icons if possible, use text everywhere else.
     if [ -n "$(ls /usr/share/fonts/OTF/Font\ Awesome*.otf)" ]; then
         pkg_symbol=""
-        update_symbol=" "
+        update_symbol="  "
     else
         pkg_symbol="PKGs"
-        update_symbol=" "
+        update_symbol=" ^ "
     fi
 
-    # Add parenthesis around the update number if there are updates to apply.
-    if [ "${updates}" -gt 0 ]; then
-        updates="(${updates})"
     # If there aren't any updates, make the relevant variables empty.
-    else
+    if [ "${updates}" -eq 0 ]; then
         updates=""
-        update_symbol=" "
+        update_symbol=""
     fi
 
     printf "%s\n" "${pkg_symbol} $(pacman -Q | wc -l)${update_symbol}${updates}"
