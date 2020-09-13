0d311359c5049cdc55f4750973de89925bf3f6f3
takelley1 <hxyz@protonmail.com>
2020-09-13 17:25:03 -0400

    Update asd notification status bar script

--- .config/i3/scripts/status-bar/notify-auto-screenshot.sh
+++ .config/i3/scripts/status-bar/notify-auto-screenshot.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env dash
 #
-# Status bar script to print a notification if asd.sh isn't running.
+# Status bar script to print a notification if the automatic-screenshot-daemon
+#   isn't running.
 
-if [ -z "$(pgrep -f "${HOME}/scripts/bash/linux/systemd/asd\.sh$")" ]; then
+if [ -z "$(pgrep -f "${HOME}/.*asd\.sh$")" ]; then
     printf "%s\n" "Screenshot script not running!"
 fi
 
