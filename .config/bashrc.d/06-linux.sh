e07eb7dd83cdc4bc184c5132ad6598eb35706fb7
takelley1 <hxyz@protonmail.com>
2020-09-30 19:25:30 -0400

    Order bashrc files by number so they are sourced in correct order

--- .config/bashrc.d/06-linux.sh
+++ .config/bashrc.d/06-linux.sh
@@ -7,17 +7,10 @@
 # shellcheck disable=2154
 #
 
-if [[ "${OSTYPE}" == "linux"* ]]; then
+if [[ "${OSTYPE}" == "linux-gnu" ]]; then
 
-
-    # Reattach to the last tmux session or create a new one if it doesn't exist.
-    #   Requires "new-session -n $HOST" in ~/.tmux.conf file.
-    #   Only runs if tmux isn't already attached and if not using tty2.
-    if [[ ! "${USER}" == "root" && -z "${TMUX}" && ! "${tty}" == "/dev/tty2" ]]; then
-        if hash tmux 2>/dev/null; then
-            exec tmux -f "${HOME}/.config/tmux/tmux.conf" attach
-        fi
-    fi
+    # asd.service breaks if this isn't enabled.
+    xhost +
 
     export SHELL="/bin/bash"
 
