831c603ad1e982296a01a50759093c5a95980d35
takelley1 <hxyz@protonmail.com>
2020-10-03 22:42:39 -0400

    Add missing quotes to prompt.sh bashrc file

--- .config/bashrc.d/08-prompt.sh
+++ .config/bashrc.d/08-prompt.sh
@@ -4,10 +4,10 @@
 #
 # Configuration for main bash prompt.
 
-if [[ ${USER} == "root" ]]; then
-    # Make root's prompt red to easily distinguish root from a standard user.
+if [[ "${USER}" == "root" ]]; then
+    # Make root prompts red.
     PS1='\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 else
-    # Make standard users' prompts green.
+    # Make standard user prompts green.
     PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 fi
