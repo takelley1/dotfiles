417a33a4e8cfdbe17db4828815653a890efbce97
takelley1 <hxyz@protonmail.com>
2020-10-12 22:42:01 -0400

    Update screenshot script comments

--- .config/i3/scripts/screenshot.sh
+++ .config/i3/scripts/screenshot.sh
@@ -2,13 +2,14 @@
 set -eu
 
 # Takes a screenshot, compresses it, and saves it to ~/Pictures.
-# Requires pngcrush and ImageMagick.
+# Requires pngcrush, ImageMagick, and optionally dunst for notifications.
 
 time="$(date +%Y-%m-%d_%H:%M:%S)"
+hostname="$(hostname)"
 
 import -window root "/tmp/screenshot_${time}.png"
-notify-send -u low "Saved screenshot to ~/Pictures/${time}_$(hostname)_screenshot.png."
-pngcrush -s "/tmp/screenshot_${time}.png" "${HOME}/Pictures/${time}_$(hostname)_screenshot.png"
+notify-send -u low "Saved screenshot to ${HOME}/Pictures/${time}_${hostname}_screenshot.png."
+pngcrush -s "/tmp/screenshot_${time}.png" "${HOME}/Pictures/${time}_${hostname}_screenshot.png"
 rm -f -- "/tmp/screenshot_${time}.png"
 
 exit 0
