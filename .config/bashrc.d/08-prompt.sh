3804a23d7df03f53a20e526c27d6933de5ec35e9
takelley1 <hxyz@protonmail.com>
2020-11-15 23:11:15 -0500

    Make server bash prompts yellow

--- .config/bashrc.d/08-prompt.sh
+++ .config/bashrc.d/08-prompt.sh
@@ -8,6 +8,11 @@ if [[ "${USER}" == "root" ]]; then
     # Make root prompts red.
     PS1='\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 else
-    # Make standard user prompts green.
-    PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    if [[ "${HOSTNAME}" != "polaris" ]] && [[ "${HOSTNAME}" != "tethys" ]]; then
+        # Make standard user prompts yellow on servers.
+        PS1='\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    else
+        # Make standard user prompts green on workstations.
+        PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    fi
 fi
