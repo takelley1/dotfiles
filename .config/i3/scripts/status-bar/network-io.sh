4098fe58d90043a90571c164827dfca564b3dd38
takelley1 <hxyz@protonmail.com>
2020-12-17 00:10:42 -0500

    Rewrite network-io.sh status bar script

--- .config/i3/scripts/status-bar/network-io.sh
+++ .config/i3/scripts/status-bar/network-io.sh
@@ -5,35 +5,29 @@
 # This script is intended to be used on the secondary status bar, so
 #   keeyping the output as short as possible is not an issue.
 #
-#  fa-angle-double-up [&#xf102;]
-#  fa-angle-double-down [&#xf103;]
-#   fa-sitemap [&#xf0e8;]
+# shellcheck disable=1004
+#
+icon="I"
+in_icon=""
+out_icon=""
 
-# Polling period.
-interval=10
-
-# Reset history.
-ifstat --reset >"/dev/null" 2>&1
-# Wait to collect data.
-sleep "${interval}"s
-
-# Determine correct network interface and grab stats.
+# Determine the correct network interface.
 interface="$(ip address show scope global up | awk '{gsub(/:/,""); if(NR==1) print $2}')"
-stats=$(ifstat --noupdate "${interface}")
+# Gather raw statistics.
+stats="$(ifdata -si -bips -so -bops "${interface}")"
 
-packets_in="$(printf "%s\n" "${stats}" | awk '{if(NR==4) print $2}')"
-packets_out="$(printf "%s\n" "${stats}" | awk '{if(NR==4) print $4}')"
-
-if [ -n "$(ls /usr/share/fonts/OTF/Font\ Awesome*.otf)" ]; then
-    symbol=" "
-    in_symbol=""
-    out_symbol=""
-else
-    symbol="NET-IO"
-    in_symbol="RX"
-    out_symbol="TX"
-fi
-
-printf "%s\n" "${symbol} ${packets_in} pkt ${in_symbol}  ${packets_out} pkt ${out_symbol}"
+# Feed vars into awk script, parse, and format.
+printf "%s\n%s\n%s\n%s\n" "${icon}" "${in_icon}" "${out_icon}" "${stats}" | awk \
+'
+ {if(NR==1)icon=$1}
+ {if(NR==2)inicon=$1}
+ {if(NR==3)outicon=$1}
+ {if(NR==4)inerr=$3 " " $4 " " $5 " " $6 " " $7 " " $8}
+ {if(NR==5)inrate=$1}
+ {if(NR==6)outerr=$3 " " $4 " " $5 " " $6 " " $7 " " $8}
+ {if(NR==7)outrate=$1}
+ END \
+ {print icon " " inrate "B(" inerr ")" inicon " " outrate "B(" outerr ")" outicon}
+'
 
 exit 0
