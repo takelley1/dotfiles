ae41c7de11095d7c7b3fbf5d818112d424401842
takelley1 <hxyz@protonmail.com>
2020-12-21 00:01:27 -0500

    Fix weather.sh checking connectivity correctly

--- .config/i3/scripts/status-bar/weather.sh
+++ .config/i3/scripts/status-bar/weather.sh
@@ -10,14 +10,14 @@ wind_icon="🍃"
 humidity_icon="💧"
 
 # Check connectivity first.
-if weather_full="$(curl -s "wttr.in?format=%c%f-%w-%h\n")"; then
+if curl https://icanhazip.com 2>/dev/stdout | grep -Eq "E2Guardian|Connection refused"; then
+    exit 0
+else
+    weather_full="$(curl -s "wttr.in?format=%c%f-%w-%h\n")"
 
     wind="$(printf "%s\n" "${weather_full}" | cut -f 2 -d '-')"
     humidity="$(printf "%s\n" "${weather_full}" | cut -f 3 -d '-')"
     weather="$(printf "%s\n" "${weather_full}" | cut -f 1 -d '-')"
 
     printf "%s\n" "${weather}, ${wind_icon}${wind}, ${humidity_icon}${humidity}"
-
 fi
-
-exit 0
