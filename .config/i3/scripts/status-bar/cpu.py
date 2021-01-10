82f0a5ca0b3c1d3842f37677e97245656f6c23c6
takelley1 <hxyz@protonmail.com>
2021-01-09 20:00:54 -0500

    Fix issue with cpu.py not showing 100% used cores

--- .config/i3/scripts/status-bar/cpu.py
+++ .config/i3/scripts/status-bar/cpu.py
@@ -3,7 +3,7 @@
 # Status bar script for printing the current usage of the four most
 #   active CPU cores/threads.
 #
-# Usage is expressed in increments of 10, so a value of "5" means
+# Usage is expressed on a scale of 1-10, so a value of "5" means
 #   a core is 50% used.
 #
 # Emoji U+1F4BB 💻
@@ -20,12 +20,21 @@ def main():
     # Break up the tuple containing the usage of each CPU core.
     for core_percent in psutil.cpu_percent(interval=3, percpu=True):
 
-        core_percent = round(core_percent)
-        # Add leading zero so all numbers are two digits.
-        core_percent = "{:02d}".format(core_percent)
-        # Only show tens place.
-        core_percent = "{:.1}".format(core_percent)
-        # Create a list using the reformatted values.
+        # Round to the nearest tens place.
+        core_percent = round(core_percent, -1)
+        core_percent = int(core_percent)
+        # print("DEBUG: initial: " + str(core_percent))
+
+        # Change two-digit numbers to one digit.
+        if core_percent >= 10 and core_percent < 100:
+            # print("DEBUG: >10<100: " + str(core_percent))
+            core_percent = "{:.1}".format(str(core_percent))
+        # Change three-digit numbers (100) to two digits.
+        elif core_percent == 100:
+            # print("DEBUG: =100: " + str(core_percent))
+            core_percent = "{:.2}".format(str(core_percent))
+
+        core_percent = int(core_percent)
         core_list.append(core_percent)
 
     # Sort list by most active cores.
