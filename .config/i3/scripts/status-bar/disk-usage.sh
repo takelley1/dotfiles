7bebf61d73d2510fbe7bc2c8b31692fa77acc8e5
takelley1 <hxyz@protonmail.com>
2021-01-24 17:37:55 -0500

    Add option to show used % in disk-usage.sh

--- .config/i3/scripts/status-bar/disk-usage.sh
+++ .config/i3/scripts/status-bar/disk-usage.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env dash
-
+#
 # Status bar script for printing the amount of gigabytes free in the root partition.
-
+#
 # Font-Awesome 
 # Emoji U+1F5C3 🗃️
 icon="🗃️"
+# Whether to print (% used) in addition to gigabytes free.
+show_used=1
 
-disk_free="$(df --human-readable --local / | awk '{if(NR==2) print $4}')"
+df="$(df --human-readable --local /)"
 
-printf "%s\n" "${icon} ${disk_free}"
+disk_free="$(printf "%s\n" "${df}" | awk '{if(NR==2) print $4}')"
+
+if [ "${show_used}" -eq 1 ]; then
+    disk_used="$(printf "%s\n" "${df}" | awk '{if(NR==2) print $5}')"
+    printf "%s\n" "${icon} ${disk_free} (${disk_used} used)"
+else
+    printf "%s\n" "${icon} ${disk_free}"
+fi
 
 exit 0
