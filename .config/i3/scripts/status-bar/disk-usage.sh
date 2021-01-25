96477557ae57a087c9a058b51565e12dec3f4ff1
takelley1 <hxyz@protonmail.com>
2021-01-24 19:39:40 -0500

    Add verbose option to disk-usage.sh

--- .config/i3/scripts/status-bar/disk-usage.sh
+++ .config/i3/scripts/status-bar/disk-usage.sh
@@ -7,16 +7,24 @@
 icon="🗃️"
 # Whether to print (% used) in addition to gigabytes free.
 show_used=1
+# Whether to print "free" next to gigabytes free.
+verbose=1
 
 df="$(df --human-readable --local /)"
 
-disk_free="$(printf "%s\n" "${df}" | awk '{if(NR==2) print $4}')"
+disk_free="$(printf "%s\n" "${df}" | awk 'END{print $4}')"
+
+if [ "${verbose}" -eq 1 ]; then
+    free=" free"
+else
+    free=""
+fi
 
 if [ "${show_used}" -eq 1 ]; then
-    disk_used="$(printf "%s\n" "${df}" | awk '{if(NR==2) print $5}')"
-    printf "%s\n" "${icon} ${disk_free} (${disk_used} used)"
+    disk_used="$(printf "%s\n" "${df}" | awk 'END{print $5}')"
+    printf "%s\n" "${icon} ${disk_free}${free} (${disk_used} used)"
 else
-    printf "%s\n" "${icon} ${disk_free}"
+    printf "%s\n" "${icon} ${disk_free}${free}"
 fi
 
 exit 0
