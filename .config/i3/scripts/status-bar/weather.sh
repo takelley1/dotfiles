538c7a83e892f350afe568f3fdc142af19e77525
takelley1 <hxyz@protonmail.com>
2021-01-30 12:54:19 -0500

    Handle no connectivity better in weather.sh

--- .config/i3/scripts/status-bar/weather.sh
+++ .config/i3/scripts/status-bar/weather.sh
@@ -9,15 +9,18 @@ wind_icon="🍃"
 # Emoji U+1F4A7 💧
 humidity_icon="💧"
 
-# Check connectivity first.
-if curl https://icanhazip.com 2>/dev/stdout | grep -Eq "E2Guardian|Connection refused|Couldn't connect"; then
-    exit 0
-else
-    weather_full="$(curl -s "wttr.in?format=%c%f-%w-%h\n")"
+# First check to make sure ping doesn't fail.
+if curl https://icanhazip.com >/dev/null 2>&1; then
+    # If ping is successful, check to make sure E2G isn't blocking it.
+    if curl https://icanhazip.com 2>/dev/stdout | grep -Eq "E2Guardian"; then
+        exit 0
+    else
+        weather_full="$(curl -s "wttr.in?format=%c%f-%w-%h\n")"
 
-    wind="$(printf "%s\n" "${weather_full}" | cut -f 2 -d '-')"
-    humidity="$(printf "%s\n" "${weather_full}" | cut -f 3 -d '-')"
-    weather="$(printf "%s\n" "${weather_full}" | cut -f 1 -d '-')"
+        wind="$(printf "%s\n" "${weather_full}" | cut -f 2 -d '-')"
+        humidity="$(printf "%s\n" "${weather_full}" | cut -f 3 -d '-')"
+        weather="$(printf "%s\n" "${weather_full}" | cut -f 1 -d '-')"
 
-    printf "%s\n" "${weather}, ${wind_icon}${wind}, ${humidity_icon}${humidity}"
+        printf "%s\n" "${weather}, ${wind_icon}${wind}, ${humidity_icon}${humidity}"
+    fi
 fi
