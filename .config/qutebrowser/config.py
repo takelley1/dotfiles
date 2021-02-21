31e69ec61ecdaa2e59cdcf4aeb84ba0f62f7f2ea
takelley1 <hxyz@protonmail.com>
2021-02-20 23:02:55 -0500

    Add qutebrowser config for newsboat

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,38 @@
+config.load_autoconfig()
+
+c.content.javascript.enabled = False
+
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.grayscale.images = 0
+c.colors.webpage.darkmode.contrast = 0.0
+
+c.url.start_pages = ["https://www.qutebrowser.org/"]
+
+c.zoom.default = "140%"
+c.zoom.levels = [
+    "25%",
+    "33%",
+    "50%",
+    "67%",
+    "75%",
+    "90%",
+    "100%",
+    "105%",
+    "110%",
+    "115%",
+    "120%",
+    "125%",
+    "130%",
+    "135%",
+    "140%",
+    "145%",
+    "150%",
+    "155%",
+    "175%",
+    "200%",
+    "250%",
+]
+
+c.tabs.show = "multiple"
+c.statusbar.show = "in-mode"
+#  c.scrolling.smooth = True
