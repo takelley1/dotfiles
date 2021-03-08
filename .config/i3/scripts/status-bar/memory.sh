ca5c74ca38731b2748c103601c27844d282f4ce3
takelley1 <hxyz@protonmail.com>
2021-03-08 02:12:23 -0500

    memory.sh: add verbose option

--- .config/i3/scripts/status-bar/memory.sh
+++ .config/i3/scripts/status-bar/memory.sh
@@ -7,9 +7,16 @@
 # Nerd Fonts f85a 
 
 icon="🐏"
+verbose=1
 
-free="$(free --human | awk '{gsub(/i/,"");if(NR==2) print $7}')"
+free="$(free --si | awk '{if(NR==2) print $7/1000000}')"
 
-printf "%s\n" "${icon} ${free}"
+if [ "${verbose}" -eq 1 ]; then
+    # Show 2 decimal places.
+    printf "%s %.2f%s\n" "${icon}" "${free}" "G free"
+else
+    # Show integer only.
+    printf "%s %2d%s\n" "${icon}" "${free}" "G free" 2>/dev/null
+fi
 
 exit 0
