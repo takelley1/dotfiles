ec8c021d15d62de0bc5afea9c3fb7e79cccbeb4d
takelley1 <hxyz@protonmail.com>
2021-03-08 01:16:06 -0500

    cpu.py: add verbose option

--- .config/i3/scripts/status-bar/cpu.py
+++ .config/i3/scripts/status-bar/cpu.py
@@ -3,13 +3,14 @@
 # Status bar script for printing the current usage of the four most
 #   active CPU cores/threads.
 #
-# Usage is expressed on a scale of 1-10, so a value of "5" means
-#   a core is 50% used.
+# If verbose is False, usage is expressed on a scale of 1-10, so a
+#   value of "5" means a core is 50% used.
 #
 # Emoji U+1F4BB 💻
 # Nerd Fonts e266 
 
 icon = "💻"
+verbose = True
 
 import sys
 import psutil
@@ -20,27 +21,34 @@ def main():
     # Break up the tuple containing the usage of each CPU core.
     for core_percent in psutil.cpu_percent(interval=3, percpu=True):
 
-        # Round to the nearest tens place.
-        core_percent = round(core_percent, -1)
-        core_percent = int(core_percent)
-        # print("DEBUG: initial: " + str(core_percent))
+        if verbose:
+            # print("DEBUG: initial: " + str(core_percent))
+            core_percent = round(core_percent)
+            core_percent = str(core_percent) + "%"
 
-        # Change two-digit numbers to one digit.
-        if core_percent >= 10 and core_percent < 100:
-            # print("DEBUG: >10<100: " + str(core_percent))
-            core_percent = "{:.1}".format(str(core_percent))
-        # Change three-digit numbers (100) to two digits.
-        elif core_percent == 100:
-            # print("DEBUG: =100: " + str(core_percent))
-            core_percent = "{:.2}".format(str(core_percent))
+        # Non-verbose formatting:
+        else:
+            # Round to the nearest tens place.
+            core_percent = round(core_percent, -1)
+            core_percent = int(core_percent)
 
-        core_percent = int(core_percent)
+            # Change two-digit numbers to one digit.
+            if core_percent >= 10 and core_percent < 100:
+                # print("DEBUG: >10<100: " + str(core_percent))
+                core_percent = "{:.1}".format(str(core_percent))
+            # Change three-digit numbers (100) to two digits.
+            elif core_percent == 100:
+                # print("DEBUG: =100: " + str(core_percent))
+                core_percent = "{:.2}".format(str(core_percent))
+
+            core_percent = int(core_percent)
+
         core_list.append(core_percent)
 
-    # Sort list by most active cores.
-    core_list.sort(reverse=True)
-    # Only show the 4 most active cores.
-    del core_list[4:]
+        # Sort list by most active cores.
+        core_list.sort(reverse=True)
+        # Only show the 4 most active cores.
+        del core_list[4:]
 
     # Format output.
     core_list = str(core_list)
