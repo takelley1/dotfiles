539d33c17162a0bd9ae904c96cee52f707630ebd
takelley1 <hxyz@protonmail.com>
2021-06-17 00:00:50 -0400

    screenshot.sh: update to store pictures on share

--- .config/i3/scripts/screenshot.sh
+++ .config/i3/scripts/screenshot.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env dash
 #
-# Takes a screenshot, compresses it, and saves it to ~/Pictures.
-#
+# Takes a screenshot, compresses it, and saves it to the NFS share.
 # Requires pngcrush, ImageMagick, and optionally dunst for notifications.
 #
 set -eu
 
 time="$(date +%Y-%m-%d_%H:%M:%S)"
 hostname="$(hostname)"
+screenshot="$(mktemp)"
 
-import -window root "/tmp/screenshot_${time}.png"
-notify-send -u low "Saved screenshot to ${HOME}/Pictures/${time}_${hostname}_screenshot.png."
-pngcrush -s "/tmp/screenshot_${time}.png" "${HOME}/Pictures/${time}_${hostname}_screenshot.png"
-rm -f -- "/tmp/screenshot_${time}.png"
+import -window root "${screenshot}.png"
+pngcrush -s "${screenshot}.png" "${HOME}/pictures/misc_pictures/screenshots/polaris_laptop/${time}_${hostname}_screenshot.png"
+rm -f -- "${screenshot}.png"
+
+notify-send -u low "Saved screenshot to ${HOME}/pictures/misc_pictures/screenshots/polaris_laptop/${time}_${hostname}_screenshot.png."
 
 exit 0
