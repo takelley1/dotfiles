aa5227ad43292323b75e4f0c1a532ca11e630178
takelley1 <hxyz@protonmail.com>
2021-06-17 22:10:45 -0400

    screenshot.sh: fix issues with temporary file

--- .config/i3/scripts/screenshot.sh
+++ .config/i3/scripts/screenshot.sh
@@ -7,12 +7,13 @@ set -eu
 
 time="$(date +%Y-%m-%d_%H:%M:%S)"
 hostname="$(hostname)"
-screenshot="$(mktemp)"
+screenshot="$(mktemp --suffix=.png)"
 
-import -window root "${screenshot}.png"
-pngcrush -s "${screenshot}.png" "${HOME}/pictures/misc_pictures/screenshots/polaris_laptop/${time}_${hostname}_screenshot.png"
-rm -f -- "${screenshot}.png"
+import -window root "${screenshot}"
+notify-send -u low "Processing screenshot..."
 
+pngcrush -s "${screenshot}" "${HOME}/pictures/misc_pictures/screenshots/polaris_laptop/${time}_${hostname}_screenshot.png"
 notify-send -u low "Saved screenshot to ${HOME}/pictures/misc_pictures/screenshots/polaris_laptop/${time}_${hostname}_screenshot.png."
+rm -f -- "${screenshot}"
 
 exit 0
