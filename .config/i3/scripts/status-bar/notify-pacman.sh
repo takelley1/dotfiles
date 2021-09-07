97296c131fdebd7e03f4e6b6d1e8d990b632030f
takelley1 <hxyz@protonmail.com>
2021-09-06 22:27:09 -0400

    rewrite conditional for notify-pacman.sh

--- .config/i3/scripts/status-bar/notify-pacman.sh
+++ .config/i3/scripts/status-bar/notify-pacman.sh
@@ -3,8 +3,8 @@
 # Status bar script that notifies the user if the daily pacman update job is
 #   running so they don't reboot while it's happening.
 
-if [ -x "/usr/bin/pacman" ]; then
-    if pgrep --newest --full --list-full "aud.sh" | grep -qEv "EDITOR|vi"; then
+if hash pacman 2>/dev/null; then
+    if systemctl is-active aud.service --quiet; then
         printf "%s\n" "Updating system..."
     fi
 fi
