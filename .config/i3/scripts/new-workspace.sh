d5746a633fbd4c825eff89e79181dd46fb1df62d
takelley1 <hxyz@protonmail.com>
2021-10-23 16:04:41 -0400

    i3: create lowest-available workspace in new-workspace.sh script

--- .config/i3/scripts/new-workspace.sh
+++ .config/i3/scripts/new-workspace.sh
@@ -10,17 +10,26 @@
 #                              the current display.
 #   `new-workspace.sh create` = Create a new workspace on the current display
 #                                 and switch to it.
+# shellcheck disable=2076
 set -eu
 
-# Print workspaces, remove quotes and formatting, then sort and print the highest-numbered workspace.
-highest_workspace="$(i3-msg -t get_workspaces | jq '.[].name' | awk 'gsub(/[^0-9]/, "")' | sort --numeric | tail -1)"
+# Print workspaces, remove quotes and formatting.
+workspaces="$(i3-msg -t get_workspaces | jq '.[].name' | awk 'gsub(/[^0-9]/, "")')"
 
-# Add 1 to determine the new workspace's number.
-new_workspace=$((highest_workspace + 1))
+# Look for an available workspace number.
+# This loop will create the lowest-numbered workspace that doesn't already exist.
+for i in $(seq 1 30); do
+    if [[ ! "${workspaces}" =~ "${i}" ]]; then
+        workspace_to_create="${i}"
+        # Break out of the loop as soon as an available workspace number is
+        #   found.
+        break 2
+    fi
+done
 
 # Create and switch to the new workspace, or move window to new workspace.
 if [[ "${1}" == "create" ]]; then
-    i3-msg workspace "${new_workspace}" 1>/dev/null
+    i3-msg workspace "${workspace_to_create}" 1>/dev/null
 elif [[ "${1}" == "move" ]]; then
-    i3-msg "move container to workspace ${new_workspace}" 1>/dev/null
+    i3-msg "move container to workspace ${workspace_to_create}" 1>/dev/null
 fi
