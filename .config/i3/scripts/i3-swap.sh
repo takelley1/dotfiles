4eb55aa11ead54a053a18376d041fb84350a2a05
takelley1 <hxyz@protonmail.com>
2021-10-24 21:22:28 -0400

    i3: improve i3-swap to work from any workspace

--- .config/i3/scripts/i3-swap.sh
+++ .config/i3/scripts/i3-swap.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
-#
-# Quickly swaps a window with its counterpart on another display.
-# Only works if one window is on each display's workspace.
-#
-# Focus is kept on the main display.
-#
-# Assumes displays are oriented such that the secondary display is located to
-#   the left of the main display.
+# Swap i3 windows with other windows on other displays.
+# Ensure focus is kept on the main display in the center (DisplayPort-3).
 set -eu
 
+# Get the output for the current workspace
+workspace_output="$(i3-msg -t get_workspaces | jq '.[] | select(.focused == true) | .output')"
+
 if [[ "${1}" == "left" ]]; then
-    i3-msg "move left"  # Move workpsace to the left display.
-    i3-msg "focus left" # Focus workspace that was on the left display.
-    i3-msg "move right" # Move that workspace to the center display.
-    i3-msg "move right"
+    i3-msg "move left"  # Move window on current display to other display.
+    i3-msg "focus left" # Focus window on other display.
+    i3-msg "move right" # Move that window to the other side of that display.
+    i3-msg "move right" # Move that window to the current display.
+    if [[ ! "${workspace_output}" =~ "DisplayPort-3" ]]; then
+        i3-msg "focus left"  # Return focus to the main display.
+    fi
+
 elif [[ "${1}" == "right" ]]; then
-    i3-msg "move right"  # Move workspace to the center display.
-    i3-msg "focus right" # Focus workspace that was on the center display.
-    i3-msg "move left"   # Move that workspace to the left display.
+    i3-msg "move right"
+    i3-msg "focus right"
     i3-msg "move left"
-    i3-msg "focus right" # Return focus to the center display.
+    i3-msg "move left"
+    if [[ ! "${workspace_output}" =~ "DisplayPort-3" ]]; then
+        i3-msg "focus right"
+    fi
 fi
